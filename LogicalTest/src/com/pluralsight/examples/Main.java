d7a0d70ba39a8c103b467a51371a7bfc15649539
Nathan Tadesse <nmtades1@gmail.com>
2019-02-14 13:31:47 -0500

    Previous work in

---
+++ LogicalTest/src/com/pluralsight/examples/Main.java
@@ -0,0 +1,14 @@
+package com.pluralsight.examples;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 0;
+        int b =10;
+
+        if (a!=0 && b/a >=10)
+            System.out.println("HOW");
+        else System.out.println("Ended well with &&");
+    }
+}
