dc09389a45243a554c318f9cdbcb8c81a00e48eb
Nathan Tadesse <nmtades1@gmail.com>
2019-03-05 17:02:50 -0500

    Exceptions Mod complete

--- CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -10,6 +10,9 @@ public class Main {
 
         //enum usage with stringbuilder. 332019
         String[] statements = {
+                "add 1.0",
+                "add xx 25.0",
+                "addX 0.0 0.0",
                 "divide 100.0 50.0",
                 "add 25.0 92.0",
                 "subtract 225.0 17.0",
@@ -18,9 +21,18 @@ public class Main {
 
         CalculateHelper helper = new CalculateHelper();
         for (String statement:statements) {
-            helper.process(statement);
-            System.out.println (helper.toString());
-        }
+            try {
+
+
+                helper.process(statement);
+                System.out.println(helper.toString());
+            } catch (InvalidStatementException e){
+                System.out.println(e.getMessage());
+                if (e.getCause() != null)
+                    System.out.println("   Original exception: "+ e.getCause().getMessage());
+
+            }
+            }
     }
 
     static void useMathEquation(){
